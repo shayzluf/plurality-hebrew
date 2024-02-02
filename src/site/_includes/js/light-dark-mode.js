89fc402c7e77938ced5a59124a0dbe531a444fbd
akinorioyama <akinorioyama@users.noreply.github.com>
2024-02-03 02:53:46 +0900

    Light and dark mode, default moved to dark mode.

--- src/site/_includes/js/light-dark-mode.js
+++ src/site/_includes/js/light-dark-mode.js
@@ -2,11 +2,46 @@ const root = document.documentElement;
 const primaryColor = getComputedStyle(root).getPropertyValue('--primary-color');
 const secondaryColor = getComputedStyle(root).getPropertyValue('--secondary-color');
 const colors = [primaryColor, secondaryColor];
-
+let primaryMode = new Map(window.location.search.slice(1).split('&').map(param => param.split('=')))
+let currentMode = null;
 function switchMode() {
-  root.style.setProperty('--primary-color', colors[1]);
-  root.style.setProperty('--secondary-color', colors[0]);
-  colors.reverse();
+  if (currentMode !== "light"){
+    root.style.setProperty('--primary-color', colors[0]);
+    root.style.setProperty('--secondary-color', colors[1]);
+  } else {
+    root.style.setProperty('--primary-color', colors[1]);
+    root.style.setProperty('--secondary-color', colors[0]);
+  }
+}
+
+function setMode(aPrimaryColor) {
+  let primaryModeString;
+
+  if (primaryMode){
+    primaryModeString = primaryMode.get("mode");
+    primaryMode = null;
+    if (primaryModeString === 'light'){
+      currentMode = 'light';
+      newMode = 'Dark Mode';
+    } else if (primaryModeString === 'dark') {
+      currentMode = 'dark';
+      newMode = 'Light Mode';
+    } else {
+      currentMode = 'dark';
+      newMode = 'Light Mode';
+    }
+  } else {
+
+  }
+  if (currentMode === 'light'){
+    newMode = 'Dark Mode';
+  } else if (currentMode === 'dark') {
+    newMode = 'Light Mode';
+  } else {
+    currentMode = 'dark';
+    newMode = 'Light Mode';  
+  }
+  return newMode;
 }
 
 if (document.getElementById('nav-right')) {
@@ -14,11 +49,12 @@ if (document.getElementById('nav-right')) {
   const darkMode = document.createElement('a');
   darkMode.classList.add('top-nav-item');
   darkMode.href = '#';
-  darkMode.innerHTML = 'Dark Mode';
+  darkMode.innerHTML = setMode(primaryColor,primaryMode);  
   navRight.appendChild(darkMode);
   darkMode.addEventListener('click', function (e) {
     e.preventDefault();
     darkMode.innerHTML = darkMode.innerHTML === 'Light Mode' ? 'Dark Mode' : 'Light Mode';
+    currentMode = currentMode === 'light' ? 'dark' : 'light';    
     switchMode();
   });
 };
@@ -28,11 +64,12 @@ if (document.getElementById('mobile-nav-right')) {
   const darkMode = document.createElement('a');
   darkMode.classList.add('nav-item');
   darkMode.href = '#';
-  darkMode.innerHTML = 'Dark Mode';
+  darkMode.innerHTML = setMode(primaryColor,primaryMode);  
   mobileNavRight.appendChild(darkMode);
   darkMode.addEventListener('click', function (e) {
     e.preventDefault();
     darkMode.innerHTML = darkMode.innerHTML === 'Light Mode' ? 'Dark Mode' : 'Light Mode';
+    currentMode = currentMode === 'light' ? 'dark' : 'light';    
     switchMode();
   });
 }
